4eb56b4cae89740de6fa2c031aca2e032c43f513
QuentinDugat <quentin_dugat@hotmail.com>
2025-05-10 17:57:46 +0200

    Ajout des verification des Formulaires de connexion/Deconnexion + setup des cookie connecté / deconnecté

---
+++ scripts/validForms/connexionForm.js
@@ -0,0 +1,26 @@
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const submitButton = document.getElementById("submit");
+const emailError = document.getElementById("emailError");
+
+submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (!emailInput.value) {
+        alert("Email is required.");
+        return;
+    }
+    if (!passwordInput.value) {
+        alert("Password is required.");
+        return;
+    }
+
+    if ((emailInput.value == "quentin_dugat@hotmail.com") && (passwordInput.value == "Quejeateselo.0192")) {
+        window.location.replace("accueil.html");
+        const token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        setToken(token);
+    } else {
+        alert("Email or password is incorrect.");
+        emailError.style.display = "block";
+    }
+});
